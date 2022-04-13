990da5215e5b4ce0f5a415e3ae3adb4a893b83ee
fayyadh wafi <20FTT1463@student.pb.edu.bn>
2022-04-14 00:20:08 +0800

    responsive and profile information

--- wadtAs3/src/App.vue
+++ wadtAs3/src/App.vue
@@ -5,8 +5,18 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-<div class="">
-  <img class="box-border h-48 w-48 mx-auto" src="./assets/Studentimg.png">
+<div class="md:w-3/5 bg-gray-400 mx-auto rounded-xl p-3 md:flex space-x-4">
+  <div class="p-1 md:w-3/5 bg-white">
+  <img class="box-border w-2/5 rounded-xl mx-auto" src="./assets/Studentimg.png">
+  </div>
+  <div class="p-10">
+  <p class="md:text-xl text-3xl text-white">Muhd Fayyadh Wafiuddin</p>
+  <p class="sm:text-sm text-m text-white">Student ID: 20FTT1463</p>
+  <p class="text-m text-white">Course: Web Development</p>
+  <p class="text-m text-white">Intake: 10</p>
+  <p class="text-m text-white">Currently Studying: Politeknik Brunei (2020-in progress)</p>
+  <p class="text-m text-white">Previously Studying: CHMS</p>
+  </div>
 </div>
 </template>
 
@@ -15,7 +25,6 @@ import HelloWorld from './components/HelloWorld.vue'
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
