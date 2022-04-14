bbc14d2c2fb558aa22a81f9e7ee457a34b6daff5
20FTT1463 <20ftt1463@student.pb.edu.bn>
2022-04-14 10:58:09 +0800

    vue project installation

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+<div class="w-3/5 bg-gray-400 mx-auto rounded-xl p-4 md:flex space-x-4">
+  <img class="box-border w-2/5 rounded-xl mx-auto" src="./assets/Studentimg.png">
+  <div class="p-2">
+  <p class="text-3xl text-white">Muhd Fayyadh Wafiuddin</p>
+  <p class="text-m text-white">Student ID: 20FTT1463</p>
+  <p class="text-m text-white">Course: Web Development</p>
+  <p class="text-m text-white">Intake: 10</p>
+  <p class="text-m text-white">Currently Studying: Politeknik Brunei (2020-in progress)</p>
+  <p class="text-m text-white">Previously Studying: CHMS</p>
+  </div>
+</div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
